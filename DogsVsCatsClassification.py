25bc287251b10183e34bbd24b2c84891d7085854
Ilian Kukov <iliankukov@gmail.com>
2023-05-22 18:23:19 +0300

    Second Commit

---
+++ DogsVsCatsClassification.py
@@ -0,0 +1,173 @@
+import os
+import shutil
+import matplotlib.pyplot as plt
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import VGG16
+import tensorflow
+
+base_data_dir = r'C:\000.App_Dev\0. Projects\cats-vs-dogs-classification-master\data'
+base_dir = r'C:\000.App_Dev\0. Projects\cats-vs-dogs-classification-master'
+model_path = r'C:\000.App_Dev\0. Projects\cats-vs-dogs-classification-master/model/'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+train_cats_dir = os.path.join(train_dir, 'cats')
+test_cats_dir = os.path.join(test_dir, 'cats')
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+def copy_files() :
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(train_cats_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(validation_cats_dir, fname)
+        shutil.copyfile(src, dst)
+  
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(test_cats_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(train_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(validation_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+  
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(test_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+    
+    print(len(os.listdir(train_cats_dir)))
+    print(len(os.listdir(test_cats_dir)))
+    print(len(os.listdir(validation_cats_dir)))
+    print(len(os.listdir(train_dogs_dir)))
+    print(len(os.listdir(test_dogs_dir)))
+    print(len(os.listdir(validation_dogs_dir)))
+
+def display_results(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validatio acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validatio loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+conv_base = VGG16(weights='imagenet',
+                      include_top=False,
+                      input_shape=(150,150,3))
+conv_base.trainable = False
+
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(216, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+                optimizer=optimizers.RMSprop(lr=1e-4),
+                metrics=['acc'])
+
+print(model.summary())
+
+model.fit(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+conv_base.trainable = True
+set_trainable = False
+
+for layer in conv_base.layers:
+    if layer.name == 'block5_conv1':
+        set_trainable = True
+    if set_trainable:
+        layer.trainable = True
+    else:
+        layer.trainable = False
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-5),
+              metrics=['acc'])
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=100,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+model.save(model_path)
+display_results(history)
+
+test_loss, test_acc = model.evaluate_generator(test_generator, steps=50)
+print('test acc:', test_acc)
